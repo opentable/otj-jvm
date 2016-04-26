f28a5632816d055401fbbd06ff51fbac676fddfe
Chris Pennello <cpennello@opentable.com>
2016-04-26 08:53:14 -0700

    Adds manual test for dumping heap.

--- src/test/java/com/opentable/jvm/MemoryTest.java
+++ src/test/java/com/opentable/jvm/MemoryTest.java
@@ -1,6 +1,7 @@
 package com.opentable.jvm;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class MemoryTest {
@@ -9,5 +10,11 @@ public class MemoryTest {
         final String out = Memory.formatNmt();
         Assert.assertNotNull(out);
         System.out.print(out);
+    }
+
+    @Ignore
+    @Test
+    public void dumpHeap() {
+        Memory.dumpHeap();
     }
 }
